9b263d926cc742d9899a9e0d8bbdd2f06a0d07d8
Michael Herschbach <aptitude@Michaels-MacBook-Pro.local>
2021-10-09 14:12:32 -0500

    reorganization and flask app

---
+++ app.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct  9 13:05:24 2021
+
+@author: aptitude
+"""
+
+from flask import Flask, render_template, request, flash, url_for, redirect
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/rxRoute', methods=['GET', 'POST'])
+def rxRoute():
+    if request.method == 'POST':
+        # do stuff when the form is submitted
+        drug = request.form['drug']
+        print(drug)
+        return "<p>" + drug + "<p>"
+
+    return render_template('rx.html')
+
+@app.route('/medRoute', methods=['GET', 'POST'])
+def medRoute():
+    if request.method == 'POST':
+        return 
+
+    # show the form, it wasn't submitted
+    return render_template('med.html')
