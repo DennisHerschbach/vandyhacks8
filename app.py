89a403467debfc565b48d650bdde681b6432a36b
Michael Herschbach <aptitude@Michaels-MacBook-Pro.local>
2021-10-09 22:41:56 -0500

    FULL IMPLEMENT

--- app.py
+++ app.py
@@ -7,6 +7,14 @@ Created on Sat Oct  9 13:05:24 2021
 """
 
 from flask import Flask, render_template, request, flash, url_for, redirect
+from bs4 import BeautifulSoup as bs
+import requests
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium import webdriver
+from time import sleep
+import pandas as pd
+import os
+import csv
 
 app = Flask(__name__)
 
@@ -18,10 +26,156 @@ def index():
 @app.route('/rxRoute', methods=['GET', 'POST'])
 def rxRoute():
     if request.method == 'POST':
+        global drug
         drug = request.form['drug']
         print(drug)
-        return render_template('rxFill.html', drug=drug)
+        
+        options = webdriver.ChromeOptions()
+        options.add_experimental_option('excludeSwitches', ['enable-logging'])
+        driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
+        
+        
+            
+            
+        drug_url = "https://www.goodrx.com/" + drug
+        driver.get(drug_url)
+        sleep(5)
+        
+        soup = bs(driver.page_source, 'html.parser')
+        
+        def getBrandNames():
+            brandclicker = driver.find_element_by_xpath("//*[@id='uat-dropdown-brand']").click()
+            sleep(5)
+            brandnames = driver.find_elements_by_class_name("option-3JfJd")
+        
+            if(len(brandnames) == 0):
+                brandnames = soup.find_all('div', attrs = {"class":"label-3WJxw label_with_props_padding-1OqSo"}) 
+        
+            brandList = []
+            for i in brandnames:
+                brandList.append(i.text)
+        
+        
+            return brandList
+        
+        
+        def getFormNames():
+            formclicker = driver.find_element_by_xpath("//*[@id='uat-dropdown-form']/span").click()
+            sleep(5)
+            formnames = driver.find_elements_by_class_name("option-3JfJd")
+        
+            if(len(formnames) == 0):
+                formnames = soup.find_all('span', attrs = {"class":"labelText-1S74- labelText_form-2mDSe labelText-1S74-"}) 
+                
+            formList = []
+            for i in formnames:
+                formList.append(i.text)
+            
+            return formList
+        
+        
+        def getDosageNames():
+            dosageclicker = driver.find_element_by_xpath("//*[@id='uat-dropdown-dosage']/span").click()
+            sleep(5)
+            dosagenames = driver.find_elements_by_class_name("option-3JfJd")
+        
+            if(len(dosagenames) == 0):
+                dosagenames = soup.find_all('span', attrs = {"class":"labelText-1S74- labelText_dosage-ChiO0"}) 
+        
+            dosageList = []
+        
+            for i in dosagenames:
+                dosageList.append(i.text)
+        
+            return dosageList
+        
+        
+        def getQuantityNames():
+            quantityclicker = driver.find_element_by_xpath("//*[@id='uat-dropdown-quantity']/span").click()
+            sleep(5)
+            quantitynames = driver.find_elements_by_class_name("option-3JfJd")
+        
+            quantityList = []
+            for i in quantitynames:
+                quantityList.append(i.text)
+        
+            return quantityList
+        
+        brands = getBrandNames()
+        forms = getFormNames()
+        dosages = getDosageNames()
+        quantitys = getQuantityNames()
+        
+        print(brands)
+        print(forms)
+        print(dosages)
+        print(quantitys)
+        
+        return render_template('rxFill.html', drug=drug, brands=brands, forms=forms, dosages=dosages, quantitys = quantitys)
     return render_template('rxFill.html')
+
+@app.route('/rxRouteOne', methods=['POST'])
+def rxRouteOne():
+    brandChoice = request.form['brandChoice']
+    formChoice = request.form['formChoice']
+    dosageChoice = request.form['dosageChoice']
+    quantityChoice = request.form['quantityChoice']
+    
+    print(brandChoice)
+    print(formChoice)
+    print(dosageChoice)
+    print(quantityChoice)
+    
+    def drugPermutations(drugName, brand, form, dosage, quantity):
+    
+        brand = brand[0:brand.index('(')-1]
+        brand = brand.lower()
+    
+        quantity = quantity[0:quantity.index(' ')]
+    
+        form = form.lower()
+        form = form.replace(' ', '-')
+        
+        specialchars = ' /'
+        for specialchar in specialchars:
+            dosage = dosage.replace(specialchar,'-')
+    
+        url = 'https://www.goodrx.com/' + drugName + '?dosage=' + dosage + '&form=' + form + '&label_override=' + brand + '&quantity=' + quantity + '&sort_type=popularity'
+        return url
+    
+    print(drug)
+    choiceUrl = drugPermutations(drug, brandChoice, formChoice, dosageChoice, quantityChoice)
+    
+    def scraper(drug_url):
+        options = webdriver.ChromeOptions()
+        options.add_experimental_option('excludeSwitches', ['enable-logging'])
+        driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
+    
+        
+        driver.get(drug_url)
+        sleep(5)
+        soup = bs(driver.page_source, 'html.parser')
+    
+    
+        allprices = soup.find_all('div', attrs={"data-qa":"drug_price"})
+        storenames = soup.find_all('span', attrs={"class":"goldAddUnderline-3T_sh"})
+        if(len(allprices) == 0):
+            return("We could not find the drug you were looking for.\n\nThis could be due to any one of the following reasons:\n1) The drug is not commercially available\n2) The drug is not approved by the FDA\n3) It is a limited distribution drug and not available at pharmacies\n4) You need a prescription for it and your healthcare provider must give you one")
+        else:
+            dict  = {'Store name': 'prices'}
+            for i in range(0,len(allprices)):
+                dict[storenames[i].text] = allprices[i].text.replace('The  price after coupon is', '').replace('The retail price  is', '')
+    
+            return dict
+
+
+        driver.quit()
+        
+    result = scraper(choiceUrl)
+    
+    print(result)
+    
+    return render_template('rxFill.html', result=result)
 
 @app.route('/medRoute', methods=['GET', 'POST'])
 def medRoute():
@@ -65,3 +219,6 @@ def medRoute():
         
         return render_template('medFill.html', treatment=treatment, priceList=priceList, cvs=cvs, kroger=kroger, walmart=walmart, costco=costco, walgreens=walgreens)
     return render_template('medFIll.html')
+
+if __name__=='__main__':
+    app.run(debug=True)
